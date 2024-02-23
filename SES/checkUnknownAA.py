3bd8886407954bbcb0fe3110c1ed5d0e3f83ccc2
Jing1Bo1Liang <158210573+Jing1Bo1Liang@users.noreply.github.com>
2024-02-23 11:04:06 -0700

    Add files via upload

---
+++ SES/checkUnknownAA.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import Bio
+from Bio.PDB import *
+from dictionary import *
+#This is used to check if there are unknown AA
+print("enter the code of the pdb(e.g. 8acy):")
+protein = input()#code of pdb in lowercase
+print("enter the file address of the pdb(e.g. /home/pdb1a23.ent):")
+address = input()#folder address like "/home/pdb1a23.ent"
+
+# parser a protein
+struc_dict = PDBParser(QUIET=True).get_structure(protein,address)
+
+#get all atoms info with non-standard
+atoms = Selection.unfold_entities(struc_dict, "A")     
+
+c_atoms=list(set([item.get_parent().get_resname() for item in atoms]))
+
+c_atoms=[item for item in c_atoms if not(item in NON_POLYMER) and not(item in k)]
+if(len(c_atoms))==0:
+    print("There is no unknown AA, go to the next step")
+else:
+    print("There are unknown AAs, please editting them in dictionary.py")
+    print("These unknown AAs are:")
+    for a in c_atoms:
+        print(a)
+
