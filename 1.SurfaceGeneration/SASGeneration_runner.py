b93f791168b105b1b32fc074b2cf681508605cae
Jing1Bo1Liang <158210573+Jing1Bo1Liang@users.noreply.github.com>
2025-06-04 09:42:32 -0600

    Update and rename SASGeneration_runner.py to SASGeneration_runner.py

--- 1.SurfaceGeneration/SASGeneration_runner.py
+++ 1.SurfaceGeneration/SASGeneration_runner.py
@@ -1,19 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from VoxelProt.surfaceGeneration.SASFeature2CSV import *
 pdb_address = "/../masif/split_proteins"
 feature_address = "/../masif/features/"
 dx_address = "/../dx/"
 slice_index = (0,10)
 SESGeneration(pdb_address,feature_address,dx_address,slice_index)
-
-
-# In[ ]:
-
-
-
-
