94abf9e2428bbfa929b67610633d7d7617d98515
Jing1Bo1Liang <158210573+Jing1Bo1Liang@users.noreply.github.com>
2025-06-09 11:25:19 -0600

    Add files via upload

---
+++ surfaceGeneration/SASGeneration_runner.py
@@ -0,0 +1,10 @@
+
+from VoxelProt.surfaceGeneration.SASFeature2CSV import *
+pdb_address = "/../masif/split_proteins"
+feature_address = "/../masif/features/"
+dx_address = "/../dx/"
+slice_index = (0,10)
+SESGeneration(pdb_address,feature_address,dx_address,slice_index)
+
+
+
