494d024b6feaff64f5da77396df9218b39454e11
Jing1Bo1Liang <158210573+Jing1Bo1Liang@users.noreply.github.com>
2025-11-17 13:14:01 -0700

    Delete bash/trainmodel.sh

--- bash/trainmodel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#change to the directory
-cd ../3.trainModel/
-echo the first argument is a list of proteins
-echo the second argument is the directory where binding site Octrees are stored
-echo the third argument is the directory where non-binding site Octrees are stored
-echo the fourth argument is where the trained model will be stored
-echo the fifth argument is the number of epoches
-python trainmodel.py $1 $2 $3 $4 $5
