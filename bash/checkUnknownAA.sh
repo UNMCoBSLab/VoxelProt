cf952c265867012d5cbf7e0f00d9d3253242d9bb
Jing1Bo1Liang <158210573+Jing1Bo1Liang@users.noreply.github.com>
2025-11-17 13:12:52 -0700

    Delete bash/checkUnknownAA.sh

--- bash/checkUnknownAA.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#change to the directory
-cd ../1.SurfaceGeneration/
-echo the first argument is the code of pdb in lowercase
-echo the second argument is the file address of the pdb
-#run the python script
-python checkUnknownAA.py $1 $2
-
-
